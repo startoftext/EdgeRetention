b3d5f6b149a31b11c14cd72b7618a27c662d34a1
James Pearson <james@xorfox.com>
2019-10-23 08:51:05 -0600

    Work in progress commit to more to laptop.

--- generated_model_pipeline.py
+++ generated_model_pipeline.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.kernel_approximation import Nystroem
-from sklearn.linear_model import RidgeCV
+from sklearn.ensemble import ExtraTreesRegressor, RandomForestRegressor
+from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVR
+from sklearn.tree import DecisionTreeRegressor
 from tpot.builtins import StackingEstimator, ZeroCount
 from xgboost import XGBRegressor
 
@@ -15,16 +17,20 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=1234)
 
-# Average CV score on the training set was:-1793.0023079905532
+# Average CV score on the training set was:0.8525458142633526
 exported_pipeline = make_pipeline(
-    VarianceThreshold(threshold=0.005),
-    Nystroem(gamma=0.9, kernel="linear", n_components=10),
-    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=True, max_features=0.5, min_samples_leaf=13, min_samples_split=3, n_estimators=100)),
+    StackingEstimator(estimator=LassoLarsCV(normalize=True)),
+    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=True, max_features=0.25, min_samples_leaf=13, min_samples_split=11, n_estimators=100)),
+    SelectPercentile(score_func=f_regression, percentile=20),
+    StackingEstimator(estimator=LinearSVR(C=0.1, dual=True, epsilon=0.001, loss="squared_epsilon_insensitive", tol=0.1)),
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=False, max_features=0.25, min_samples_leaf=2, min_samples_split=10, n_estimators=100)),
+    SelectPercentile(score_func=f_regression, percentile=20),
     ZeroCount(),
-    ZeroCount(),
-    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=True, max_features=0.5, min_samples_leaf=13, min_samples_split=17, n_estimators=100)),
-    StackingEstimator(estimator=RidgeCV()),
-    XGBRegressor(learning_rate=0.1, max_depth=2, min_child_weight=4, n_estimators=100, nthread=1, objective="reg:squarederror", subsample=0.9500000000000001)
+    StandardScaler(),
+    StackingEstimator(estimator=XGBRegressor(learning_rate=1.0, max_depth=5, min_child_weight=15, n_estimators=100, nthread=1, objective="reg:squarederror", subsample=0.4)),
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=False, max_features=0.25, min_samples_leaf=2, min_samples_split=5, n_estimators=100)),
+    StackingEstimator(estimator=DecisionTreeRegressor(max_depth=1, min_samples_leaf=18, min_samples_split=4)),
+    LassoLarsCV(normalize=True)
 )
 
 exported_pipeline.fit(training_features, training_target)
--- tpot_model.py
+++ tpot_model.py
@@ -10,7 +10,8 @@ from sklearn import decomposition
 
 rand_state = 1234
 
-pathToData = "cut_data_cleaned - cut tests 20 degree.csv"
+# pathToData = "cut_data_cleaned - cut tests 20 degree.csv"
+pathToData = "cut_data_cleaned.csv"
 data = pd.read_csv(pathToData)
 print("Data head")
 print(data.head())
@@ -23,14 +24,14 @@ y = data.Cuts
 X = data[[
           'C (Carbon)', 'Cr (Chromium)', 'Co (Cobalt)', 'Cu (Copper)', 'Mn (Manganese)', 'Mo (Molybdenum)',
           'N (Nickel)', 'Nb (Niobium)', 'N (Nitrogen)', 'Phosphorous (P)', 'Si (Silicon)', 'Sulfur (S)',
-          'Ti (titanium)', 'W (Tungsten)', 'V (Vanadium)']]
+          'Ti (titanium)', 'W (Tungsten)', 'V (Vanadium)', 'Edge angle']]
 
 # Before scaling or normalizing we want to impute missing values
 # In this case the only missing values should be the edge angle that is missing in some cases
 # TODO I could try mean, median and most frequent for imputing missing values
 # For now i do not need the imputer because I have limited the data to 20 degree angles
-# imputer = preprocessing.Imputer(strategy='median')
-# X = imputer.fit_transform(X)
+imputer = preprocessing.Imputer(strategy='median')
+X = imputer.fit_transform(X)
 
 # Scale our features (tpot only does StandardScaler and I need to scale to do PCA later)
 print("\nMean of X before scaling")
@@ -68,13 +69,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_s
 tpot = TPOTRegressor(generations=150,
                      population_size=250,
                      verbosity=2,
-                     n_jobs=-1,
+                     n_jobs=-2,
                      periodic_checkpoint_folder='tpot_checkpoints',
                      memory='auto',
                      cv=5,
                      mutation_rate=0.8,
                      crossover_rate=0.2,
-                     random_state=rand_state)
+                     random_state=rand_state,
+                     scoring='r2')
 tpot.fit(X_train, y_train)
 
 print("\nTest data score:"+str(tpot.score(X_test, y_test))+"\n")
